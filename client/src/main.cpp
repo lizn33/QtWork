b3be5dca408fa39a20ffd2fc136e71a853421efd
ZuoTiJia <2239651886@qq.com>
2022-06-14 10:33:21 +0800

    separate client server

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include "test/test.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    testMysqlpp();
     MainWindow w;
     w.show();
+
+
     return a.exec();
 }
